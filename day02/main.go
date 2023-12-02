1680586d24f8b94b6883de9ce01569b78dc1016a
ultima-thule <joanna.grzywna@gmail.com>
2023-12-02 15:10:02 +0100

    Day 1 puzle 2 refactoring

--- day02/main.go
+++ day02/main.go
@@ -8,90 +8,86 @@ import (
 	"strconv"
 )
 
-func readFile(filename string) int {
+func readFile(filename string) *os.File {
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
-		return 0
+		return nil
 	}
-	defer f.Close()
-
-	fileScanner := bufio.NewScanner(f)
-	fileScanner.Split(bufio.ScanLines)
-
-	var maxGreen, maxRed, maxBlue int = 13, 12, 14
-	var sumGames int
-
-	for fileScanner.Scan() {
-		game := matchStr(fileScanner.Text(), "Game (\\d+):")
-
-		green := matchStr(fileScanner.Text(), "(\\d+) green")
-		red := matchStr(fileScanner.Text(), "(\\d+) red")
-		blue := matchStr(fileScanner.Text(), "(\\d+) blue")
-
-		if green <= maxGreen && red <= maxRed && blue <= maxBlue {
-			sumGames += game
-		}
-
-		fmt.Println(fileScanner.Text(), " => G: ", green, " R: ", red, "B: ", blue)
-	}
-	return sumGames
+	return f
 }
 
-func readFile2(filename string) int {
-	f, err := os.Open(filename)
+func closeFile(f *os.File) {
+	err := f.Close()
 	if err != nil {
 		fmt.Println(err)
-		return 0
+		os.Exit(1)
 	}
-	defer f.Close()
-
-	fileScanner := bufio.NewScanner(f)
-	fileScanner.Split(bufio.ScanLines)
-
-	var gamesPower, sumGames int
-
-	for fileScanner.Scan() {
-		green := matchStr(fileScanner.Text(), "(\\d+) green")
-		red := matchStr(fileScanner.Text(), "(\\d+) red")
-		blue := matchStr(fileScanner.Text(), "(\\d+) blue")
-
-		gamesPower = green * red * blue
-
-		sumGames += gamesPower
-
-		// fmt.Println(fileScanner.Text(), " => G: ", green, " R: ", red, "B: ", blue)
-	}
-	return sumGames
 }
 
-func matchStr(text string, patt string) int {
+func findMax(text string, patt string) int {
 	pattern := regexp.MustCompile(patt)
-	var greater, numb int
+	var greater int
 
 	matches := pattern.FindAllStringSubmatch(text, -1)
-	// fmt.Printf("Matches %v", matches)
-	// fmt.Println("Text: ", text, " Matches: ", pattern.NumSubexp(), " first: ", matches[0], " - ", matches[1])
 
 	for _, v := range matches {
-		// fmt.Printf("%d. %s => %s\n", k, v, v[1])
-		numb, _ = strconv.Atoi(v[1])
-		greater = max(numb, greater)
+		numb, err := strconv.Atoi(v[1])
+		if err == nil {
+			greater = max(numb, greater)
+		}
 	}
 
 	return greater
 }
 
+func extractData(text string) (int, int, int, int) {
+	game := findMax(text, "Game (\\d+):")
+
+	green := findMax(text, "(\\d+) green")
+	red := findMax(text, "(\\d+) red")
+	blue := findMax(text, "(\\d+) blue")
+
+	return game, green, red, blue
+}
+
+func cubeConudrum(filename string, calcFun func(int, int, int, int) int) int {
+	fmt.Println("=> DataSet: ", filename)
+
+	f := readFile(filename)
+	defer closeFile(f)
+
+	fileScanner := bufio.NewScanner(f)
+	fileScanner.Split(bufio.ScanLines)
+
+	var result int
+
+	for fileScanner.Scan() {
+		game, green, red, blue := extractData(fileScanner.Text())
+		result += calcFun(game, green, red, blue)
+	}
+	return result
+}
+
+func calcSum(game int, green int, red int, blue int) int {
+	const maxGreen, maxRed, maxBlue int = 13, 12, 14
+
+	if green <= maxGreen && red <= maxRed && blue <= maxBlue {
+		return game
+	}
+	return 0
+}
+
+func calcPower(game int, green int, red int, blue int) int {
+	return green * red * blue
+}
+
 func main() {
-	fmt.Println("---- Test DataSet1 ----")
-	fmt.Println(readFile("input_test1.txt"))
+	fmt.Println(cubeConudrum("input_test1.txt", calcSum))
 
-	fmt.Println("---- DataSet 1 ----")
-	fmt.Println(readFile("input1.txt"))
+	fmt.Println(cubeConudrum("input1.txt", calcSum))
 
-	fmt.Println("---- Test DataSet 2 ----")
-	fmt.Println(readFile2("input_test2.txt"))
+	fmt.Println(cubeConudrum("input_test2.txt", calcPower))
 
-	fmt.Println("---- DataSet 2 ----")
-	fmt.Println(readFile2("input2.txt"))
+	fmt.Println(cubeConudrum("input2.txt", calcPower))
 }
